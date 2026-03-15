ae0987e5f9fe299c5750c45570097bfe92cb7366
LeGoatest <ironduckydesigns@gmail.com>
2026-03-15 16:22:15 -0500

    Create content.js

---
+++ assets/js/content.js
@@ -0,0 +1,17 @@
+export const TASKS_CONFIG = {
+  combat: {
+    name: 'Hunt',
+    baseDuration: 2000,
+    station: './views/combat.html'
+  },
+  woodcutting: {
+    name: 'Woodcutting',
+    baseDuration: 3000,
+    station: './views/gathering.html'
+  },
+  mining: {
+    name: 'Mining',
+    baseDuration: 4000,
+    station: './views/gathering.html'
+  }
+};
