6f1b304085d205b2dd1ddce0fd49f95fc3121916
LeGoatest <ironduckydesigns@gmail.com>
2026-03-16 18:49:32 -0500

    Update main.go

--- cmd/content-validate/main.go
+++ cmd/content-validate/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/LeGoatest/Pwa-idle-game/internal/content"
+)
 
 func main() {
-	fmt.Println("content validate stub")
+	reg, err := content.LoadRegistry("./content")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("content valid\n")
+	fmt.Printf("items: %d\n", len(reg.ItemsList))
+	fmt.Printf("skills: %d\n", len(reg.SkillsList))
+	fmt.Printf("zones: %d\n", len(reg.ZonesList))
+	fmt.Printf("monsters: %d\n", len(reg.MonstersList))
+	fmt.Printf("shop items: %d\n", len(reg.ShopItemsList))
+	fmt.Printf("drop tables: %d\n", len(reg.DropTablesList))
 }
