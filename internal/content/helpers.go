0233c3dcddd002d1377cc0ebd6e454d8d017309d
LeGoatest <ironduckydesigns@gmail.com>
2026-03-16 18:44:38 -0500

    Update helpers.go

--- internal/content/helpers.go
+++ internal/content/helpers.go
@@ -20,6 +20,16 @@ func GetSkill(reg *Registry, id string) (Skill, bool) {
 	return skill, ok
 }
 
+func GetShopItem(reg *Registry, id string) (ShopItem, bool) {
+	item, ok := reg.ShopItems[id]
+	return item, ok
+}
+
+func GetDropTable(reg *Registry, id string) (DropTable, bool) {
+	table, ok := reg.DropTables[id]
+	return table, ok
+}
+
 func GetItemsByTag(reg *Registry, tag string) []Item {
 	out := make([]Item, 0)
 	for _, item := range reg.ItemsList {
