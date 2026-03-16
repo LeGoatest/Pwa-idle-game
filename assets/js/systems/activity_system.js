81ea5c8d4ef5698f258625b9e6994143ce1469d7
LeGoatest <ironduckydesigns@gmail.com>
2026-03-15 19:14:15 -0500

    Create activity_system.js

---
+++ assets/js/systems/activity_system.js
@@ -0,0 +1,17 @@
+export function startActivity(state, payload) {
+
+  state.activity = {
+    ...payload,
+    startedAt: Date.now(),
+    lastProcessedAt: Date.now(),
+    progress: 0
+  }
+}
+
+export function stopActivity(state) {
+
+  state.activity = {
+    kind: "none",
+    progress: 0
+  }
+}
